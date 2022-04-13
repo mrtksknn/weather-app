c96e5bc7638216f1fadef0447cc52a15e9ea3313
Mert keskin <mertkeskinmtk6@gmail.com>
2022-04-13 14:40:21 +0300

    little fixes

--- src/components/Container.js
+++ src/components/Container.js
@@ -4,12 +4,7 @@ import Header from "./Header";
 
 const Container = () => {
   return (
-    <div style={{
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems: 'center',
-      height: '100vh'}}>
+    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
       <Header />
       <Body />
     </div>
